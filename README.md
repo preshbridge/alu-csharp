59019a829ac16587454c86af4d71686ffe07ca0a
preshbridge <b.ogbuagu@alustudent.com>
2025-01-09 18:13:16 +0100

    3-writeline

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # alu-csharp
-<<<<<<< HEAD
 2025
-=======
-csharp-hello_world
->>>>>>> 457dd9e365bfc31a21111b0ea30d4728364c2ac0
---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // This line prints text to the console
+        Console.WriteLine("Hello, World!");
+
+        // You can print more lines like this:
+        Console.WriteLine("This is another message!");
+    }
+}
