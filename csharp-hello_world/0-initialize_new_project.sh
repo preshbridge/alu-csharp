91cd931542b5f3b80838024ec8f35cf30be4e7ef
Bridget Chimdinma <139658311+preshbridge@users.noreply.github.com>
2025-01-09 17:42:53 +0100

    Update 0-initialize_new_project.sh

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Create a folder titled 0-new_project
-mkdir -p 0-new_project
 
-# Navigate into the folder
-cd 0-new_project || exit
 
-# Initialize a new C# project
-dotnet new console
+# Initialize a new C# Console Application project
+4
+dotnet new console -n 0-neu project
 
-# Return to the original directory
-cd ..
+
+
+# Navigate into the newly created project directory
+
+cd 0-new project
+8
+
+9
+* Restore the required packages
+10
+dotnet restore
+11
+
+12
+# Display success message
+13
+
+13 lines (9 loc) • 336 Bytes
+echo The new C# project has been created and packages restored successfully.-
