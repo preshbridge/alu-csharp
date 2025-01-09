0d39349e3bc6ac37bc6614fc6274e570c97fc195
Bridget Chimdinma <139658311+preshbridge@users.noreply.github.com>
2025-01-09 17:54:17 +0100

    Update 0-initialize_new_project.sh

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,27 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# this is gonna initialize a new project
 
-
-
-# Initialize a new C# Console Application project
-4
-dotnet new console -n 0-neu project
-
-
-
-# Navigate into the newly created project directory
-
-cd 0-new project
-8
-
-9
-* Restore the required packages
-10
-dotnet restore
-11
-
-12
-# Display success message
-13
-
-13 lines (9 loc) • 336 Bytes
-echo The new C# project has been created and packages restored successfully.-
+dotnet new console -o 0-new_project -f "netcoreapp2.1"
