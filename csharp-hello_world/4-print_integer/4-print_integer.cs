2d26d40cd528f8fde535ca2d2a92f6c53871de82
preshbridge <b.ogbuagu@alustudent.com>
2025-01-11 23:00:24 +0100

    Updated .NET 7.0 project files

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		int number = 972;
-        Console.WriteLine(number + " Mission Street");
-	}
+    static void Main(string[] args)
+    {
+        int number = 972; // Initialize the number variable
+        Console.WriteLine(number + " Mission Street"); // Print the integer followed by the text
+    }
 }
