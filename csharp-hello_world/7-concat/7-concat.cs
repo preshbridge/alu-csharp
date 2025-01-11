fa8f155231468654ac1d13b52a85d122a034f4bf
preshbridge <b.ogbuagu@alustudent.com>
2025-01-11 20:55:13 +0100

    7-concat

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,11 @@
+using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str1 = "Holberton";
+		string str2 = "School";
+		str1 += " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
+	}
+}
