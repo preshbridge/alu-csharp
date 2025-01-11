f51aa4cf7982cdf50ba76f3bfca3b18756c126b5
preshbridge <b.ogbuagu@alustudent.com>
2025-01-11 20:58:46 +0100

    0-intialize_new_project.sh

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-# Initialize a new .NET project using .NET 7.0
 
-echo "Creating project with .NET 7.0..."
-dotnet new console -o 0-new_project
-
-# Check if the project folder was created
-if [ -d "0-new_project" ]; then
-    echo "Project created successfully. Files:"
-    ls -la 0-new_project
-else
-    echo "Failed to create the project."
-fi
+mkdir 0-new_project
+cd 0-new_project
+dotnet new console
