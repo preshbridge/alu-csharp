7a5f0302d79b92f8802c42cd323b9c3708397499
preshbridge <b.ogbuagu@alustudent.com>
2025-01-11 11:23:55 +0100

    csharp-hello_world

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-# this is gonna initialize a new project
-
-dotnet new console -o 0-new_project -f "netcoreapp2.1"
+O-initialize new project.sh
+#!/bin/bash
+dongt new console -o 0-new_project
