a3212f993cb7bf12a803978a06122f616c0fc325
Bridget Chimdinma <139658311+preshbridge@users.noreply.github.com>
2025-01-11 11:25:42 +0100

    Update 0-initialize_new_project.sh

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,3 +1,4 @@
-O-initialize new project.sh
-#!/bin/bash
-dongt new console -o 0-new_project
+#!/usr/bin/env bash
+# this is gonna initialize a new project
+
+dotnet new console -o 0-new_project -f "netcoreapp2.1"
