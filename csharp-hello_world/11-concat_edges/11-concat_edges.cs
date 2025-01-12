869a3344d372c262cea06aeeb44c13964dd5d588
preshbridge <b.ogbuagu@alustudent.com>
2025-01-12 08:32:50 +0100

    11-concat_edge

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string p1 = "object", p2 = "-oriented ", p3 = "programming", p4 = " in C#";
+        string result = p1 + p2 + p3 + p4;
+        Console.WriteLine(result);
+    }
+}
