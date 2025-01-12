5724cee657d4740b42f3c1c5734fcc585f55741a
preshbridge <b.ogbuagu@alustudent.com>
2025-01-12 07:13:11 +0100

    5-print_float

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program
 {
-	static void Main(string[] args)
-        {
-		float number = 3.14159f;
-        Console.WriteLine("Float: " + number.ToString("0.00"));
-        }
+    static void Main(string[] args)
+    {
+        float number = 3.14159f;
+        Console.WriteLine("Float: {0:F2}", number);
+    }
 }
