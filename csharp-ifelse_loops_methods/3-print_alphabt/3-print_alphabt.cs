b17b4ee995251e87652f3b1c5a4676604f6d5a12
preshbridge <b.ogbuagu@alustudent.com>
2025-01-12 17:08:54 +0100

    3-print_alphabt

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            if (letter != 'q' && letter != 'e')
+            {
+                Console.Write(letter);
+            }
+        }
+    }
+}
