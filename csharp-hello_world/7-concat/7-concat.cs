ca5aa1cd13a05d1486072c6bb16c4d155102a180
preshbridge <b.ogbuagu@alustudent.com>
2025-01-12 07:51:02 +0100

    Welcome to Mission Street!

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,8 +6,7 @@ class Program
     {
         string str1 = "Welcome to";
         string str2 = " Holberton School!";
-        string str3 = " Mission Street";
-        string result = str1 + str2 + str3; // Concatenate str1,str2 and str3
+        string result = str1 + str2; // Concatenate str1 and str2
         Console.WriteLine(result);   // Print the result
     }
 }
