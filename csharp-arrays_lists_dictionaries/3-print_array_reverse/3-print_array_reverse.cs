f3fabdd07c3c24ba21205d768a85905c9876910a
preshbridge <b.ogbuagu@alustudent.com>
2025-01-19 18:48:01 +0100

    3-prinit_reverse

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+            if (i > 0)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
