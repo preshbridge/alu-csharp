d2fa2591e2451364435e8dd0d413efb7fb823074
preshbridge <b.ogbuagu@alustudent.com>
2025-01-24 12:38:20 +0100

    9-add_key_value

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+
+class Dictionary{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict.ContainsKey(key))
+        {
+            myDict[key] = value;
+        }
+        else{
+            myDict.Add(key, value);
+        }
+        
+        return myDict;
+    }
+}
