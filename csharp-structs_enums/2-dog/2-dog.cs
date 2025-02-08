1a81e16bffe9a5615e02e166ebd33d05d62c5381
preshbridge <b.ogbuagu@alustudent.com>
2025-02-08 08:53:05 +0200

    2-dog

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,22 @@
+using System;
+
+public enum Rating{
+    Good,
+    Great, 
+    Excellent,
+}
+
+
+public struct Dog {
+
+    public Dog(string name, float age, string owner, Rating rating){
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
