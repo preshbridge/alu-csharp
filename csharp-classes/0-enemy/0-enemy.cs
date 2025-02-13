ab9f33397df87e1744a5c06ab5b1965e1ac06ce8
preshbridge <b.ogbuagu@alustudent.com>
2025-02-13 21:07:42 +0200

    csharp-classes

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Zombie is an empty class
+    /// </summary>
+    public class Zombie
+    {
+
+    }
+}
---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Enemies {
+    /// <summary>
+    /// Zombie is a public class 
+    /// </summary>
+    public class Zombie {
+
+        /// <summary>
+        /// Holds the value of health
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Zombie Constructor inits health to zero
+        /// </summary>
+        public Zombie() {
+            health = 0;
+        }
+    }
+}
