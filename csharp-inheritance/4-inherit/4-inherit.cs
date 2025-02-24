7aa56fff5f58efb2b9879e85de907d8a126e2cf5
preshbridge <b.ogbuagu@alustudent.com>
2025-02-24 14:42:57 +0200

    4-inherit

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,17 @@
+using System;
+
+/// <summary>
+/// Represents the most basic form of an animal.
+/// </summary>
+public class Animal
+{
+    // Empty class
+}
+
+/// <summary>
+/// Represents a dog, which is a derivative of an animal.
+/// </summary>
+public class Dog : Animal
+{
+    // Empty class
+}
